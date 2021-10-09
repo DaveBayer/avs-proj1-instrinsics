ea4592f0e43eb0b5b581c058ef563a7cc889b991
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-10-09 17:30:19 +0200

    asdf

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -49,7 +49,7 @@ static inline __m512i mandelbrot(__m512 real, __m512 imag, int limit)
 		const __m512 i2 = _mm512_mul_ps(zImag, zImag);
 
 	//	if (r2 + i2 > 4.0f) then write i to result
-		__mmask16 test_mask = _mm512_cmp_ps_mask(_mm512_add_ps(r2, i2), four, _CMP_GT_OQ);
+		__mmask16 test_mask = _mm512_cmp_ps_mask(_mm512_add_ps(r2, i2), four, _CMP_GT_OS);
 
 		result = _mm512_mask_mov_epi32(result, test_mask & result_mask, _mm512_set1_epi32(i));
 		__mmask16 res_mask_old = result_mask;
