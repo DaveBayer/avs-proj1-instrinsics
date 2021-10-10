7374479941806e7e63dea5762d22274ae03ae429
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-10-10 09:35:16 +0200

    fda

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -31,10 +31,10 @@ LineMandelCalculator::~LineMandelCalculator() {
 	data = nullptr;
 }
 
-static inline __m512i mandelbrot(__m512 real, __m512 imag, int limit)
+static inline __m512i mandelbrot(__m512 real, __m512 imag, int limit, __mmask16 mask)
 {
 	__m512i result = _mm512_setzero_epi32();
-	__mmask16 result_mask = 0xffffU;
+	__mmask16 result_mask = mask;
 
 	const __m512 two = _mm512_set1_ps(2.f);
 	const __m512 four = _mm512_set1_ps(4.f);
@@ -50,7 +50,8 @@ static inline __m512i mandelbrot(__m512 real, __m512 imag, int limit)
 
 		_mm512_storeu_ps(tmp, _mm512_add_ps(r2, i2));
 		for (int i = 0; i < 16; i++) {
-			std::cout << tmp[i] << " ";
+			if ((1U << i) & mask)
+				std::cout << tmp[i] << " ";
 		}
 		std::cout << std::endl;
 		break;
@@ -122,6 +123,12 @@ int * LineMandelCalculator::calculateMandelbrot () {
 		__m512 y = _mm512_set1_ps(y_start + i * dy);
 
 		for (int j = 0, *col_ptr = row_ptr; j < width; j += AVX512_SIZE_PS, col_ptr += AVX512_SIZE_PS) {
+			int diff = width - j;
+			__mmask16 mask = 0xffffU;
+			
+			if (diff < AVX512_SIZE_PS)
+				mask >>= AVX512_SIZE_PS - diff;
+
 			__m512d j1_pd = _mm512_add_pd(_mm512_set1_pd(static_cast<double>(j)), inc_pd);
 			__m512d j2_pd = _mm512_add_pd(_mm512_set1_pd(static_cast<double>(j + AVX512_SIZE_PD)), inc_pd);
 
@@ -130,17 +137,11 @@ int * LineMandelCalculator::calculateMandelbrot () {
 			__m512d x2_pd = _mm512_add_pd(x_start_pd, _mm512_mul_pd(j2_pd, dx_pd));
 			__m512 x = _mm512_concat_ps256(_mm512_cvtpd_ps(x1_pd), _mm512_cvtpd_ps(x2_pd));
 
-			__m512i values = mandelbrot(x, y, limit);
+			__m512i values = mandelbrot(x, y, limit, mask);
 
 
 		//	store values in memory pointed by col_ptr using mask
-			int diff = width - j;
-			__mmask16 store_mask = 0xffffU;
-			
-			if (diff < AVX512_SIZE_PS)
-				store_mask >>= AVX512_SIZE_PS - diff;
-
-			_mm512_mask_storeu_epi32(col_ptr, store_mask, values);
+			_mm512_mask_storeu_epi32(col_ptr, mask, values);
 		}
 
 		std::cout << std::endl;
