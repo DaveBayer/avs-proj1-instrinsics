d6453f8afca1849a12272062c7c98f8965cafd12
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-10-11 17:57:35 +0200

    asdf

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -11,12 +11,12 @@ VALID=1
 echo "Reference vs line"
 python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_line.npz  || VALID=0
 
-#echo "Reference vs batch"
-#python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_line.npz || VALID=0
+echo "Reference vs batch"
+python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_line.npz || VALID=0
 
 
-#echo "Batch vs line"
-#python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_line.npz cmp_batch.npz || VALID=0
+echo "Batch vs line"
+python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_line.npz cmp_batch.npz || VALID=0
 
 if [ "$VALID" -eq 1 ]; then
     echo "Test passed";
