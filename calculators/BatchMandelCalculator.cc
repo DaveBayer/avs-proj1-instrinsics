50520d2858df039a22c609981de5ae1e6507d224
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-10-16 12:48:41 +0200

    asdf

--- calculators/BatchMandelCalculator.cc
+++ calculators/BatchMandelCalculator.cc
@@ -16,7 +16,7 @@
 
 #include "BatchMandelCalculator.h"
 
-#if defined(__AVX512F__) && defined(__AVX512DQ__)
+#if 0 //defined(__AVX512F__) && defined(__AVX512DQ__)
 #	pragma message("Using AVX512F & AVX512DQ")
 #	define MM_ALIGNMENT 64
 #	define MM_SIZE_32BIT 16
